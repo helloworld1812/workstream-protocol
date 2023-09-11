97bdfbf2f9329af627ab939a28de081d2bbfe611
vidhya211992 <85571337+vidhya211992@users.noreply.github.com>
2023-09-11 14:45:10 +0530

    feat: Workstream Protocol: Define core employee onboarding module messages[WS-25563] (#7)
    * added new message CoreEmployeeOnboardingModuleEvent
    
    * updated message name

--- packages/sdk-ruby/src/workstream_protocol/lib/workstream_protocol/Onboarding_pb.rb
+++ packages/sdk-ruby/src/workstream_protocol/lib/workstream_protocol/Onboarding_pb.rb
@@ -139,6 +139,19 @@ Google::Protobuf::DescriptorPool.generated_pool.build do
     optional :updated_at, :message, 15, "google.protobuf.Timestamp"
     optional :deleted_at, :message, 16, "google.protobuf.Timestamp"
   end
+  add_message "Workstream.Protocol.Onboarding.WSCoreEmployeeOnboardingEvent" do
+    optional :id, :string, 1
+    optional :company_id, :string, 2
+    optional :employee_id, :string, 3
+    optional :created_at, :message, 4, "google.protobuf.Timestamp"
+    optional :updated_at, :message, 5, "google.protobuf.Timestamp"
+    optional :deleted_at, :message, 6, "google.protobuf.Timestamp"
+    optional :status, :string, 7
+    optional :type, :string, 8
+    optional :completed_at, :message, 9, "google.protobuf.Timestamp"
+    optional :last_restarted_at, :message, 10, "google.protobuf.Timestamp"
+    optional :manually_completed_by, :string, 11
+  end
 end
 
 module Workstream
@@ -154,6 +167,7 @@ module Workstream
       EmployeeInitialStateEvent = Google::Protobuf::DescriptorPool.generated_pool.lookup("Workstream.Protocol.Onboarding.EmployeeInitialStateEvent").msgclass
       LocationEvent = Google::Protobuf::DescriptorPool.generated_pool.lookup("Workstream.Protocol.Onboarding.LocationEvent").msgclass
       W4Event = Google::Protobuf::DescriptorPool.generated_pool.lookup("Workstream.Protocol.Onboarding.W4Event").msgclass
+      WSCoreEmployeeOnboardingEvent = Google::Protobuf::DescriptorPool.generated_pool.lookup("Workstream.Protocol.Onboarding.WSCoreEmployeeOnboardingEvent").msgclass
     end
   end
 end
